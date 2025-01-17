b198c00651f3a0ba6579f3cc76f3dbd8f363b608
Abhishek Singh <abhisheksingh4115@gmail.com>
2025-01-17 11:59:29 +0000

    updated add passeneger section

--- src/components/booking/AddPassenger.jsx
+++ src/components/booking/AddPassenger.jsx
@@ -78,174 +78,174 @@ const AddPassenger = () => {
 	return (
     <div>
       <Header />
-      <div className="min-h-screen bg-white p-4">
-        {/* Button Section */}
-        <div className="flex justify-center gap-4 mb-8">
-          <button
-            onClick={() => navigate("/dashboard")}
-            className="px-5 py-2 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400 transition flex items-center"
-          >
-            <TiArrowBack className="mr-2" />
-            <span>Back</span>
-          </button>
+      <div className=" bg-white p-4 min-h-[500px] max-h-[70vh] overflow-y-auto sm:min-h-screen sm:max-h-full">
+      {/* Button Section */}
+      <div className="flex justify-center gap-2 sm:gap-4 mb-6 sm:mb-8">
+        <button
+          onClick={() => navigate("/dashboard")}
+          className="px-3 py-2 sm:px-5 sm:py-2 bg-gray-300 text-gray-700 text-xs sm:text-sm rounded-lg hover:bg-gray-400 transition flex items-center"
+        >
+          <TiArrowBack className="mr-1 sm:mr-2" />
+          <span>Back</span>
+        </button>
 
-          <button
-            onClick={() => navigate("/add-passenger")}
-            className="px-5 py-2 bg-[#b91c1c] text-white rounded-lg hover:bg-red-700 transition"
-          >
-            Add Passenger
-          </button>
-          <button
-            onClick={() => navigate("/passengerlist")}
-            className="px-5 py-2 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400 transition"
-          >
-            Passenger List
-          </button>
-        </div>
+        <button
+          onClick={() => navigate("/add-passenger")}
+          className="px-3 py-2 sm:px-5 sm:py-2 bg-[#b91c1c] text-white text-xs sm:text-sm rounded-lg hover:bg-red-700 transition"
+        >
+          Add Passenger
+        </button>
+        <button
+          onClick={() => navigate("/passengerlist")}
+          className="px-3 py-2 sm:px-5 sm:py-2 bg-gray-300 text-gray-700 text-xs sm:text-sm rounded-lg hover:bg-gray-400 transition"
+        >
+          Passenger List
+        </button>
+      </div>
 
-        {/* Add Passenger Form */}
-        <div className="flex justify-center">
-          <div className="w-full max-w-2xl bg-white p-8 rounded-lg shadow-lg overflow-auto">
-            <form onSubmit={handleSubmit} className="space-y-6">
-              {/* Passenger Name */}
+      {/* Add Passenger Form */}
+      <div className="flex justify-center">
+        <div className="w-full max-w-md sm:max-w-2xl bg-white p-4 sm:p-8 rounded-lg shadow-lg overflow-auto">
+          <form onSubmit={handleSubmit} className="space-y-4 sm:space-y-6">
+            {/* Passenger Name */}
+            <div>
+              <label
+                htmlFor="passengerName"
+                className="block text-xs sm:text-sm text-gray-700 font-medium mb-1"
+              >
+                Passenger Name:
+                <span className="text-red-500 ml-1">*</span>
+              </label>
+              <input
+                type="text"
+                id="passengerName"
+                placeholder="Enter passenger name"
+                value={formData.passengerName}
+                onChange={handleChange}
+                className="w-full px-3 py-2 sm:px-4 sm:py-3 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring text-xs sm:text-sm"
+                required
+              />
+            </div>
+
+            {/* Description */}
+            <div>
+              <label
+                htmlFor="description"
+                className="block text-xs sm:text-sm text-gray-700 font-medium mb-1"
+              >
+                Description:
+                <span className="text-red-500 ml-1">*</span>
+              </label>
+              <input
+                type="text"
+                id="description"
+                placeholder="Enter description"
+                value={formData.description}
+                onChange={handleChange}
+                className="w-full px-3 py-2 sm:px-4 sm:py-3 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring text-xs sm:text-sm"
+                required
+              />
+            </div>
+
+            {/* Address and Postcode */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 sm:gap-4">
               <div>
                 <label
-                  htmlFor="passengerName"
-                  className="block text-gray-700 font-medium mb-1"
+                  htmlFor="address"
+                  className="block text-xs sm:text-sm text-gray-700 font-medium mb-1"
                 >
-                  Passenger Name:
+                  Address:
                   <span className="text-red-500 ml-1">*</span>
                 </label>
                 <input
                   type="text"
-                  id="passengerName"
-                  placeholder="Enter passenger name"
-                  value={formData.passengerName}
+                  id="address"
+                  placeholder="Enter address"
+                  value={formData.address}
                   onChange={handleChange}
-                  className="w-full px-4 py-3 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-red-500"
+                  className="w-full px-3 py-2 sm:px-4 sm:py-3 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring text-xs sm:text-sm"
                   required
                 />
               </div>
 
-              {/* Description */}
               <div>
                 <label
-                  htmlFor="description"
-                  className="block text-gray-700 font-medium mb-1"
+                  htmlFor="postcode"
+                  className="block text-xs sm:text-sm text-gray-700 font-medium mb-1"
                 >
-                  Description:
+                  Postcode:
                   <span className="text-red-500 ml-1">*</span>
                 </label>
                 <input
                   type="text"
-                  id="description"
-                  placeholder="Enter description"
-                  value={formData.description}
+                  id="postcode"
+                  placeholder="Enter postcode"
+                  value={formData.postcode}
                   onChange={handleChange}
-                  className="w-full px-4 py-3 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-red-500"
+                  className="w-full px-3 py-2 sm:px-4 sm:py-3 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring text-xs sm:text-sm"
                   required
                 />
               </div>
+            </div>
 
-              {/* Address and Postcode */}
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                <div>
-                  <label
-                    htmlFor="address"
-                    className="block text-gray-700 font-medium mb-1"
-                  >
-                    Address:
-                    <span className="text-red-500 ml-1">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    id="address"
-                    placeholder="Enter address"
-                    value={formData.address}
-                    onChange={handleChange}
-                    className="w-full px-4 py-3 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-red-500"
-                    required
-                  />
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="postcode"
-                    className="block text-gray-700 font-medium mb-1"
-                  >
-                    Postcode:
-                    <span className="text-red-500 ml-1">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    id="postcode"
-                    placeholder="Enter postcode"
-                    value={formData.postcode}
-                    onChange={handleChange}
-                    className="w-full px-4 py-3 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-red-500"
-                    required
-                  />
-                </div>
-              </div>
-
-              {/* Phone and Email */}
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                <div>
-                  <label
-                    htmlFor="phone"
-                    className="block text-gray-700 font-medium mb-1"
-                  >
-                    Phone #:
-                  </label>
-                  <input
-                    type="tel"
-                    id="phone"
-                    placeholder="Enter phone number"
-                    value={formData.phone}
-                    onChange={handleChange}
-                    className="w-full px-4 py-3 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-red-500"
-                  />
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-gray-700 font-medium mb-1"
-                  >
-                    Email:
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    placeholder="Enter email address"
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="w-full px-4 py-3 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-red-500"
-                  />
-                </div>
-              </div>
-
-              {/* Buttons */}
-              <div className="flex justify-end space-x-4">
-                <button
-                  type="button"
-                  onClick={() => navigate("/passengerlist")}
-                  className="px-5 py-2 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400"
+            {/* Phone and Email */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 sm:gap-4">
+              <div>
+                <label
+                  htmlFor="phone"
+                  className="block text-xs sm:text-sm text-gray-700 font-medium mb-1"
                 >
-                  Cancel
-                </button>
-                <button
-                  type="submit"
-                  className="px-5 py-2 bg-black text-white rounded-lg"
-                  disabled={loading} // Disable button while loading
-                >
-                  {loading ? "Submitting..." : "Create Passenger"}
-                </button>
+                  Phone #:
+                </label>
+                <input
+                  type="tel"
+                  id="phone"
+                  placeholder="Enter phone number"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 sm:px-4 sm:py-3 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring text-xs sm:text-sm"
+                />
               </div>
-            </form>
-          </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-xs sm:text-sm text-gray-700 font-medium mb-1"
+                >
+                  Email:
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Enter email address"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 sm:px-4 sm:py-3 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring text-xs sm:text-sm"
+                />
+              </div>
+            </div>
+
+            {/* Buttons */}
+            <div className="flex flex-col sm:flex-row justify-end gap-2 sm:gap-4">
+              <button
+                type="button"
+                onClick={() => navigate("/passengerlist")}
+                className="px-3 py-2 sm:px-5 sm:py-2 bg-gray-300 text-gray-700 text-xs sm:text-sm rounded-lg hover:bg-gray-400"
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="px-3 py-2 sm:px-5 sm:py-2 bg-black text-white text-xs sm:text-sm rounded-lg"
+                disabled={loading} // Disable button while loading
+              >
+                {loading ? "Submitting..." : "Create Passenger"}
+              </button>
+            </div>
+          </form>
         </div>
       </div>
+    </div>
     </div>
   );
 };
